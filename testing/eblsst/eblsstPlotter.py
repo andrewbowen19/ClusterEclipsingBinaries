f22c5cbd995bf93461ce24434a637d9d2851b331
Andrew Bowen <atbowen@gmail.com>
2020-06-30 15:35:14 -0500

    Cleaned EBLSST Hist code

--- testing/eblsst/eblsstPlotter.py
+++ testing/eblsst/eblsstPlotter.py
@@ -1,7 +1,9 @@
 # Script to create plots from output eblsst files
 # These files are grabbed from different observing scenarios
-# They are produced from the analyseClusterLISA script located in the flex directory
+# eblsst files from the analyseClusterLISA script in the flex directory
 # Let's plot!
+# info on VRO filters here: 
+# https://community.lsst.org/t/lsst-filter-profiles/1463
 
 import numpy as np
 import matplotlib.pyplot as plt
@@ -11,73 +13,81 @@ import re
 
 
 class eblsstPlotter(object):
-	'''
-	Class to create histograms for 
-	'''
-	def __init__(self, DataFrame, clusterType, strategy, crowding, param):
-		self.DataFrame = DataFrame
-		self.clusterType = clusterType
-		self.strategy = strategy
-		self.param = param  # binary parameter to be plotted
-		# Filter plotting colors '<filter name>': '<matplotlib color>'
-		# More info here: https://community.lsst.org/t/lsst-filter-profiles/1463
-		self.filterColors = {'u': '#8B00FF', 'g': '#0000FF',
-							 'r': '#00FF00', 'i': '#FFFF00',
-							 'z': '#FF7F00', 'y': '#FF0000',
-							 'ugrizy': '#000000'}
-		# Dictionary including parameter keys (from file names to more descriptive labels)
-		self.paramDict = {'lp': 'log-period', 'd': 'dist',
-						  'm1': 'm1', 'q': 'q',
-						  'mag': 'mag', 'e': 'e', 'r': 'r'}
-		self.crowding = crowding
+    '''
+    Class to create histograms for different binary parameters
+    This histogram data is pulled from EBLSST output files
+    Parameters:
+    DataFrame: pandas df style object pulled from csv files
+    clusterType: either 'GlobularClusters', 'OpenClusters', 'm10', 'm67'
+    crowding: str, 'Crowd' or 'NoCrowd'
+    strategy: str, 'base', 'crowd'
+    The string inputs are case-sensitive
+    '''
+    def __init__(self, DataFrame, clusterType, strategy, crowding, param):
+        self.DataFrame = DataFrame
+        self.clusterType = clusterType
+        self.strategy = strategy
+        self.param = param  # binary parameter to be plotted
+        # Filter plotting colors '<filter name>': '<matplotlib color>'
+        self.filterColors = {'u': '#8B00FF', 'g': '#0000FF',
+                             'r': '#00FF00', 'i': '#FFFF00',
+                             'z': '#FF7F00', 'y': '#FF0000',
+                             'ugrizy': '#000000'}
+        # Dictionary including param keys (from file names)
+        self.paramDict = {'lp': 'log-period', 'd': 'dist',
+                          'm1': 'm1', 'q': 'q',
+                          'mag': 'mag', 'e': 'e', 'r': 'r'}
+        self.crowding = crowding
 
-	def makeIndHist(self, data, filter, show_mean=False, save_fig=True):
-		'''
-		Method to generate histograms of binaries for individual Vera Rubin filters
-		data: list-like containing binaries in each bin (should be a 'hist' column from eblsst files)
-		bins: bins for hist, use 'binsEdges' columnn from csv files
-		'''
-		print(f'Making Individual histogram for {self.clusterType}-{self.strategy}-{self.param}...')
-		print(f'filter: {filter}')
-		f, ax = plt.subplots(figsize=(8, 5))
-		ax.hist(data, bins=self.DataFrame['binEdges'], histtype='step', color=self.filterColors[filter])
-		ax.set_title(self.clusterType + '-' + self.strategy + self.crowding + '; ' + filter)
-		ax.set_xlabel(self.param)#.replace('EBLSST_', ''))
+    def makeIndHist(self, data, filter, show_mean=False, save_fig=True):
+        '''
+        Generates histograms of binary paramsfor individual filters
+        data: list-like containing binaries in each bin 
+            (should be a 'hist' column from eblsst files)
+        bins: bins for hist, use 'binsEdges' columnn from csv files
+        '''
+        print(f'Making Individual histogram for {self.clusterType}-{self.strategy}-{self.param}...')
+        print(f'filter: {filter}')
+        f, ax = plt.subplots(figsize=(8, 5))
+        ax.hist(data, bins=self.DataFrame['binEdges'],
+                histtype='step', color=self.filterColors[filter])
+        ax.set_title(self.clusterType + '-' + self.strategy + self.crowding + '; ' + filter)
+        ax.set_xlabel(self.param)
 
-		if show_mean:
-			ax.vline(np.mean(data), linestyle='--', label='Distribution mean')
+        if show_mean:
+            ax.vline(np.mean(data), linestyle='--', label='Distribution mean')
 
-		if save_fig:
-			fileName = f'{self.clusterType}-{self.strategy}-{self.param}-{filter}-histInd.pdf'
-			path_to_fig = os.path.join('.', 'plots', self.clusterType, self.strategy + self.crowding, 'ind', fileName)
-			f.savefig(path_to_fig, format='pdf')
-		# f.close()
-		print('Individual histogram complete!')
+        if save_fig:
+            fileName = f'{self.clusterType}-{self.strategy}-{self.param}-{filter}-histInd.pdf'
+            path_to_fig = os.path.join('.', 'plots', self.clusterType, self.strategy + self.crowding, 'ind', fileName)
+            f.savefig(path_to_fig, format='pdf')
 
+        print('Individual histogram complete!')
 
+    def makeCombinedHist(self, filters, save_fig=True):
+        '''
+        Method to make histogram of binaries including filters
+        data: just pass dataframe with filter columns to this
+        filters: list of strings containinf filter keys
+        '''
+        multiData = self.DataFrame.drop(['binEdges', 'histAll', 'histObs', 'allhistRec'],
+                                        axis=1)
 
-	def makeCombinedHist(self, filters, save_fig=True):
-		'''
-		Method to make histogram of binaries including filters
-		data: just pass dataframe with filter columns to this
-		filters: list of strings containinf filter keys
-		'''
-		multiData = self.DataFrame.drop(['binEdges', 'histAll', 'histObs', 'allhistRec'], axis=1)
-		
-		fig, ax = plt.subplots(figsize=(8, 5))
+        fig, ax = plt.subplots(figsize=(8, 5))
+        # Looping thru filters
+        for f in filters:
+            ax.hist(multiData[f+'_histRec'], bins=self.DataFrame['binEdges'],
+                    histtype='step', linestyle='--',
+                    color=self.filterColors[f], label=f, alpha=0.7)
+        ax.legend()
+        ax.set_title(ax.set_title(self.clusterType + '-' + self.strategy + self.crowding + '; recovered'))
+        ax.set_xlabel(self.paramDict[self.param])  # replacing parameter label from file name w param key (in dict above)
+        ax.set_ylabel(r'$N_{bin}$')
 
-		for f in filters:
-			ax.hist(multiData[f+'_histRec'], bins=self.DataFrame['binEdges'],
-							  histtype='step', linestyle='--', color=self.filterColors[f], label=f, alpha=0.7)
-		ax.legend()
-		ax.set_title(ax.set_title(self.clusterType + '-' + self.strategy + self.crowding + '; recovered'))
-		ax.set_xlabel(self.paramDict[self.param])  # replacing parameter label from file name w param key (in dict above)
-		ax.set_ylabel(r'$N_{bin}$')
-
-		if save_fig:
-			filterStr = ''.join(filters)
-			fileName = f'{self.clusterType}-{self.strategy}-{self.param}-{filterStr}-histCombined.pdf'
-			path_to_fig = os.path.join('.', 'plots', self.clusterType, self.strategy + self.crowding, 'combined', fileName)
-			fig.savefig(path_to_fig)
+        if save_fig:
+            filterStr = ''.join(filters)
+            fileName = f'{self.clusterType}-{self.strategy}-{self.param}-{filterStr}-histCombined.pdf'
+            path_to_fig = os.path.join('.', 'plots', self.clusterType, self.strategy + self.crowding, 'combined', fileName)
+            fig.savefig(path_to_fig)
 
 
--- testing/eblsst/runPlotter.py
+++ testing/eblsst/runPlotter.py
@@ -1,11 +1,12 @@
 '''
-Script to run the eblsstPloter (located in this directory) for each observing scenario
+Script to run the eblsstPloter for each observing scenario
+eblsstPlotter class should reside in working dir
 The scenarios include every permutation of the following:
 Open/Globular
 baseline/colossus
 crowding/no-crowding
-Utilizes methods from eblsstPlotter to generate histograms for each scenario/binary param
-*Note: in our nomenclature, OS refers to the 'observing scenario'. Sorry for any confusion.
+Utilizes methods from eblsstPlotter to generate histograms for each scenario
+*Note: in our nomenclature, OS refers to the 'observing scenario'.
 '''
 
 from eblsstPlotter import eblsstPlotter
@@ -15,10 +16,12 @@ import pandas as pd
 import os
 import re
 
-
-# dictionary including path folder names and the respective OpSim strategy and crowding scenario
-scenarioDict = {'baseCrowd': ['base', 'Crowd'], 'baseNoCrowd': ['base', 'NoCrowd'],
-				'colCrowd': ['col', 'Crowd'], 'colNoCrowd': ['col', 'NoCrowd']}
+# dictionary including path folder names
+# Also includes respective OpSim strategy and crowding scenario
+scenarioDict = {'baseCrowd': ['base', 'Crowd'],
+				'baseNoCrowd': ['base', 'NoCrowd'],
+				'colCrowd': ['col', 'Crowd'],
+				'colNoCrowd': ['col', 'NoCrowd']}
 filtersLSST = ['u', 'g', 'r', 'i', 'z', 'y']
 
 # Looping through all observing scenarios
@@ -30,7 +33,7 @@ for root, dirs, files in os.walk(path_to_clusters, topdown=True):
 		if 'eblsst_files' in d:
 			scenario = root.replace('./clusters/', '')
 			print('Here\'s the sitch:', scenario)
-			path_to_files = os.path.join(root , d)
+			path_to_files = os.path.join(root, d)
 
 			# Defining observing scenario (OC/GC, base/col, crowd/NoCrowd)
 			cluster = scenario.split('/')[0]
@@ -38,7 +41,7 @@ for root, dirs, files in os.walk(path_to_clusters, topdown=True):
 			strategyOpSim = scenarioDict[stratCrowd][0]  # either baseline or colossus
 			crowding = scenarioDict[stratCrowd][1]
 			print(f'Making plots for following OS: {cluster}, {strategyOpSim}, {crowding}...')
-			
+
 			# Generating plots for each binary parameter file in working OS
 			for file in os.listdir(path_to_files):
 
@@ -46,11 +49,11 @@ for root, dirs, files in os.walk(path_to_clusters, topdown=True):
 				binParam = file.replace('EBLSST_', '').replace('hist.csv', '')
 				eb = eblsstPlotter(df, cluster, strategyOpSim, crowding, binParam)
 				eb.makeCombinedHist(filtersLSST, True)
-				
 
+				# Making individual filter histograms
 				for c in df.columns:
 
-					# Getting filter key from column titles (if an individual filter col)
+					# Getting filter key from column titles
 					# If there is an individual filter column, it's recovered
 					if '_histRec' in c:
 						filt = c.replace('_histRec', '')
@@ -60,8 +63,6 @@ for root, dirs, files in os.walk(path_to_clusters, topdown=True):
 
 
 print("All done!")
-
-
 
 
 
